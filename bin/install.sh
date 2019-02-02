772b32e2aa9c5ff9cb5897da27bbd55c4bc541ad
Maksim Gayduk <maksim.gayduk@gmail.com>
2019-02-02 11:23:30 +0200

    Created basic vim config

--- bin/install.sh
+++ bin/install.sh
@@ -3,6 +3,9 @@
 CONF_PATH=`git rev-parse --show-toplevel`/config
 
 # git
-ln -sf $CONF_PATH/git/.gitconfig $HOME/.gitconfig
+ln -sf $CONF_PATH/git/gitconfig $HOME/.gitconfig
+
+# vim
+ln -sf $CONF_PATH/vim/vimrc $HOME/.vimrc
 
 echo "Config installing is finished"
