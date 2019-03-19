b491933866b684c53112538cd93782c1c0717034
Maksim Gayduk <maksim.gayduk@gmail.com>
2019-03-19 10:34:51 +0200

    Created basic psql config

--- bin/install.sh
+++ bin/install.sh
@@ -8,4 +8,7 @@ ln -sf $CONF_PATH/git/gitconfig $HOME/.gitconfig
 # vim
 ln -sf $CONF_PATH/vim/vimrc $HOME/.vimrc
 
+# psql
+ln -sf $CONF_PATH/psql/psqlrc $HOME/.psqlrc
+
 echo "Config installing is finished"
